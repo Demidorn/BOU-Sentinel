a912ecffbb277b6bb034c0173d64cc4959080ce1
Ikilia Doreen <ikilaidoreen@gmail.com>
2026-06-17 17:21:15 +0300

    Add MapLayout component with Sidebar for map layout

---
+++ BOU /apps/web/app/map/layout.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function MapLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="ml-64 flex-1 p-6">{children}</main>
+    </div>
+  );
+}
